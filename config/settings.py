eabc8fffdd786bcc1723c76d3c3a44add00eeffc
pablo <pablogeorgeporras@gmail.com>
2022-09-18 03:45:25 -0700

    update settings

--- config/settings.py
+++ config/settings.py
@@ -48,14 +48,18 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+CORS_ALLOWED_ORIGINS = [
+"https://domain.com",
+"https://api.domain.com",
+"http://localhost:5000",
+"http://127.0.0.1:9000"
+]
 
 ROOT_URLCONF = 'config.urls'
 
