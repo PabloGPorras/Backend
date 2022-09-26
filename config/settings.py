8fd79ea1421de863f1263202952074ef93c28aba
pablo <pablogeorgeporras@gmail.com>
2022-09-26 13:09:20 -0700

    add cors http://127.0.0.1:5173/

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,9 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:5000",
 "http://127.0.0.1:9000",
 "https://tea-party.vercel.app/",
-"https://tea-party.vercel.app"
+"https://tea-party.vercel.app",
+"http://127.0.0.1:5173/",
+"http://127.0.0.1:5173"
 ]
 
 ROOT_URLCONF = 'config.urls'
