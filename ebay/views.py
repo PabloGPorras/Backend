a19aa036d37a382aac03fad3ed67dc16aad8c6e9
pablo <pablogeorgeporras@gmail.com>
2022-09-28 23:14:02 -0700

    working on mongo insert into

--- ebay/views.py
+++ ebay/views.py
@@ -14,13 +14,6 @@ from django.shortcuts import redirect
 import json
 from pymongo import MongoClient
 
-#MONGO DB CONNECTION
-username="ebay-django-db"
-password = "E8KiR0WoJ8IACyKe"
-connect_string  =f"mongodb+srv://{username}:{password}@cluster0.cly8mna.mongodb.net/?retryWrites=true&w=majority"
-my_client = MongoClient(connect_string)
-# First define the database name
-dbname = my_client["tea-party"]
 
 
 def index(request):
@@ -85,7 +78,14 @@ def authAccepted(request):
 
         _________________________
         """)
-
+        
+        #MONGO DB CONNECTION
+        username="ebay-django-db"
+        password = "E8KiR0WoJ8IACyKe"
+        connect_string  =f"mongodb+srv://{username}:{password}@cluster0.cly8mna.mongodb.net/?retryWrites=true&w=majority"
+        my_client = MongoClient(connect_string)
+        # First define the database name
+        dbname = my_client["tea-party"]
         # Now get/create collection name (remember that you will see the database in your mongodb cluster only after you create a collection)
         collection_name = dbname["user-login"]
         sellerInfo ={
