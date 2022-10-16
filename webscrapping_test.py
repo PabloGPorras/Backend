3c5d17a269d28974f703955698dbda5f55a90355
pablo <pablogeorgeporras@gmail.com>
2022-10-16 12:43:19 -0700

    testing

--- webscrapping_test.py
+++ webscrapping_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.common.keys import Keys
 import os
 
 chrome_options = webdriver.ChromeOptions()
@@ -12,5 +12,24 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
 
 # Now you can start using Selenium
-driver.get("http://www.google.com")
-print(driver.page_source)
+driver.get("https://www.walmart.com/account/login?vid=oaoh&tid=0&returnUrl=%2F")
+
+#ENTER EMAIL
+driver.find_element_by_xpath('//*[@id="email-split-screen"]').send_keys('pablamos.porras@gmail.com')
+
+#SIGN IN AFTER EMAIL
+driver.find_element_by_xpath('//*[@id="sign-in-with-email-validation"]/div[1]/div/button').click()
+
+#USE PASSWORD RADIO BUTTON
+driver.find_element_by_xpath('//*[@id="enterPwd"]').click()
+
+#ENTER PASSWORD
+driver.find_element_by_xpath('//*[@id="sign-in-password-change"]').send_keys('@Pablamos1725')
+
+#CLICK SIGN IN 
+driver.find_element_by_xpath('//*[@id="sign-in-with-pwd-form"]/div[5]/button').click()
+
+
+#CONFIRM LOGGED IN 
+test = driver.find_element_by_xpath('//*[@id="__next"]/div[1]/div/span/header/div[3]/a/div/div[1]').text
+print(test)
